8e4d1ee02b088c776d6b71d0084e71d6893b0e73
keom-loma <keom.heang@lomatechnology.com>
2025-01-06 14:09:38 +0700

    Add files via upload
    First init

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select the button
+const alertButton = document.getElementById("alertButton");
+
+// Add a click event listener
+alertButton.addEventListener("click", () => {
+  alert("Hello! You clicked the button.");
+});
